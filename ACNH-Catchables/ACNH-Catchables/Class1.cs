f99fbf2bdc7ec694a3cf07eeb80a6811b9f187a2
Deren B <dbozerprogramming@gmail.com>
2021-11-20 01:22:39 -0500

    Testing

---
+++ ACNH-Catchables/ACNH-Catchables/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACNH_Catchables {
+    internal class Class1 {
+        public static void Main(string[] args) {
+            Console.WriteLine("All your base are belong to us.");
+            string variableOne = "crackers";
+            Console.WriteLine($"Sprinkle some {variableOne} on him.");
+        }
+    }
+}
